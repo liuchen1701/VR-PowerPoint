ca8137890311f307d82eadb55b7c1ff180c86589
gajeelSF <yus167@UCSD.EDU>
2017-03-03 23:27:38 -0800

    workable on both daydream and vive

--- Assets/ShowPPT.cs
+++ Assets/ShowPPT.cs
@@ -2,48 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ShowPPT : MonoBehaviour {
 
     public GameObject model;
 
-    // the working controller
-    public GameObject thisController;
-    int controllerIndex;
+    private int startSlide;
+    private int endSlide;
 
     // the image urls for ppt
-    private String[] urls;
+    public String[] urls;
     // the index of currently showing image
-    private int index;
 
     public GameObject display;
-
-    // display control flags
-    private Boolean clicked;    
-    private Boolean right;
 
     private String numSlidesURL = "http://ec2-54-67-100-135.us-west-1.compute.amazonaws.com/numslides";
 
     private int numSlides;
-
-    private int startSlide;
-    private int endSlide;
 
     void Start()
     {
         model.SetActive(false);
 
         //_controller = GetComponent<SteamVR_TrackedController>();
-        
-        index = -1;
-  
-        clicked = false;
-        right = true;
-        
+                
         StartCoroutine(init());
-
-        // update changing once per second
-        InvokeRepeating("change", 0, 0.8f);
+        
 
     }
 
@@ -69,25 +54,31 @@ public class ShowPPT : MonoBehaviour {
             Debug.Log(urls[i]);
         } 
 
-        StartCoroutine(showPPT());
     }
 
-    void change()
-    {
-        if (!(SteamVR_Controller.Input(controllerIndex).GetPressDown(SteamVR_Controller.ButtonMask.Axis0)) && clicked)
-        {
-            print(clicked);
-            if (clicked)
-            {
-                clicked = false;
-                StartCoroutine("showPPT");
+    
 
-            }
-        }
+    
+
+
+    IEnumerator changeSlide(int index)
+    {
+
+        //Debug.Log(index);
+        String url = urls[index];
+        
+        // get the url of the image
+        WWW www = new WWW(url);
+       
+        yield return www;
+
+        //Debug.Log("showPPt");
+
+        //update the display screen using the image as the texture
+        display.GetComponent<Renderer>().material.mainTexture = www.texture;
 
         // change model state
-
-        if(index >= startSlide && index <= endSlide)
+        if (index >= startSlide && index <= endSlide)
         {
             model.SetActive(true);
         }
@@ -95,78 +86,16 @@ public class ShowPPT : MonoBehaviour {
         {
             model.SetActive(false);
         }
-
-    }
-
-    void Update()
-    {
-        
-        // get which controller is working
-        controllerIndex = (int)thisController.GetComponent<SteamVR_TrackedObject>().GetDeviceIndex();
-
-        
-        if (SteamVR_Controller.Input(controllerIndex).GetPress(SteamVR_Controller.ButtonMask.Axis0) && !clicked )
-        {
-            Debug.Log("pressed");
-
-            // check if the user is pressing on the left side of pad or the right side
-            if( SteamVR_Controller.Input(controllerIndex).GetAxis().x < 0.0f )
-            {
-                right = false;
-            }
-            else
-            {
-                right = true;
-            }
-            // update the click condition, prepare for change
-            clicked = true;
-        }
-        
-    }
-
-
-    IEnumerator showPPT()
-    {
-
-
-        if (index < urls.Length  )
-        {
-
-            //Debug.Log("left or right: "+right);
-            //Debug.Log("..." + clicked);
-
-            // going to the next silde if the last condition of user pressing is right
-            // do not change if it is the last slide
-            if (right && index < urls.Length - 1)
-                {
-                    index++;
-                    //Debug.Log("...//" + clicked);
-            }
-
-            // go to the previous slide
-            else if(!right)
-                {
-                    if (index != 0)
-                    {
-                        index--;                      
-                    }
-                }
-
-            }
-
-
-        //Debug.Log(index);
-        String url = urls[index];
-        
-        // get the url of the image
-        WWW www = new WWW(url);
-        print(index+"....");
-        yield return www;
-
-        //Debug.Log("showPPt");
-
-        //update the display screen using the image as the texture
-        display.GetComponent<Renderer>().material.mainTexture = www.texture;
-       
     } 
+
+    
+
+    [ClientRpc]
+    public void RpcSyncPPT(int index)
+    {
+
+        Debug.Log("the current ppt number: " + index);
+
+        StartCoroutine(changeSlide(index));
+    }
 }
---
+++ Assets/ShowPPT_Vive.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Networking;
+
+
+
+public class ShowPPT_Vive : MonoBehaviour {
+
+    public GameObject thisController;
+
+    int controllerIndex;
+
+    private int index;
+
+    public GameObject model;
+
+
+
+
+
+    // display control flags
+    private Boolean clicked;
+    private Boolean right;
+
+    // Use this for initialization
+    void Start () {
+        clicked = false;
+        right = true;
+        InvokeRepeating("change", 0, 0.8f);
+
+        index = -1;
+        StartCoroutine("updateSlide");
+    }
+	
+
+
+    void Update()
+    {
+
+        // get which controller is working
+        controllerIndex = (int)thisController.GetComponent<SteamVR_TrackedObject>().GetDeviceIndex();
+
+
+        if (SteamVR_Controller.Input(controllerIndex).GetPress(SteamVR_Controller.ButtonMask.Axis0) && !clicked)
+        {
+            Debug.Log("pressed");
+
+            // check if the user is pressing on the left side of pad or the right side
+            if (SteamVR_Controller.Input(controllerIndex).GetAxis().x < 0.0f)
+            {
+                right = false;
+            }
+            else
+            {
+                right = true;
+            }
+            // update the click condition, prepare for change
+            clicked = true;
+        }
+
+    }
+
+    void change()
+    {
+        if (!(SteamVR_Controller.Input(controllerIndex).GetPressDown(SteamVR_Controller.ButtonMask.Axis0)) && clicked)
+        {
+            print(clicked);
+            if (clicked)
+            {
+                clicked = false;
+            }
+        }
+
+
+       
+
+    }
+
+    public void updateSlide()
+    {
+        if (index < this.GetComponent<ShowPPT>().urls.Length)
+        {
+
+            //Debug.Log("left or right: "+right);
+            //Debug.Log("..." + clicked);
+
+            // going to the next silde if the last condition of user pressing is right
+            // do not change if it is the last slide
+            if (right && index < this.GetComponent<ShowPPT>().urls.Length - 1)
+            {
+                index++;
+                //Debug.Log("...//" + clicked);
+            }
+
+            // go to the previous slide
+            else if (!right)
+            {
+                if (index != 0)
+                {
+                    index--;
+                }
+            }
+        }
+        this.GetComponent<ShowPPT>().RpcSyncPPT(index);
+    }
+}
