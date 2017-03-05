a9495fb513149498e0ac35e8f128141cfc581b5b
Luis Calona <lcalona@ucsd.edu>
2017-03-04 20:32:54 -0800

    Added laser functionality

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    public GameObject laser;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    private GameObject reticle;
+
+	void Start ()
+    {
+        reticle = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    }
+
+    void Update ()
+    {
+        createLaser();
+        updateReticle();
+	}
+
+    void updateReticle()
+    {
+        reticle.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        reticle.transform.localPosition = endPoint;
+        reticle.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void createLaser()
+    {
+        laser.GetComponent<LineRenderer>().SetPosition(0, startPoint);
+        laser.GetComponent<LineRenderer>().SetPosition(1, endPoint);
+        laser.GetComponent<LineRenderer>().startWidth = 0.01f;
+        laser.GetComponent<LineRenderer>().endWidth = 0.01f;
+        laser.GetComponent<LineRenderer>().material.color = new Color(1.0f, 0f, 0f, 0.75f); ;
+    }
+}
---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+public class LaserPointer : MonoBehaviour
+{
+    public GameObject reticle;
+    public GameObject laser;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void toggleLaserPointer()
+    {
+ 
+    }
+}
